9f674a9a13da9ecbd88e60b02e7cd26ae3e5c53a
Geoffrey Stephenson <stephenson.geoffrey@gmail.com>
2021-01-08 22:18:08 -0500

    tweaks

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export default function App() {
 				<ColorChangeButton color="red" setColor={setColor} />
 				<ColorChangeButton color="blue" setColor={setColor} />
 				<ColorChangeButton color="yellow" setColor={setColor} />
+				<ColorChangeButton color="green" setColor={setColor} />
 			</div>
 		</div>
 	);
